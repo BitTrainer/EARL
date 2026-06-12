c0f08c30fabb3c23b6938030aa597e81f18d79c6
Bittrainer <grant.vanstaden@live.com.au>
2026-06-12 20:41:39 +1000

    Basic state management

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,10 +2,16 @@
 
 enum VehicleState {
   Stopped,
-  Driving,
+  HeadingToDestination,
   AvoidingObstacle,
-  Stuck
+  Arrived
 };
+
+enum VehicleAction{  
+  Start,
+  Stop,
+  AvoidObstacle
+ };
 
 const int BUTTON_PIN = 9;  
 VehicleState currentState = Stopped;
@@ -13,22 +19,35 @@ VehicleState currentState = Stopped;
 void setup() {
   Serial.begin(9600);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  prepareVehicle();  
+  prepareVehicle();        
+}
+
+VehicleState performAction(VehicleAction action) {
+  switch(action) {    
+    case Start:
+      Serial.println("Starting vehicle...");
+      driveForward();
+      return HeadingToDestination;
+    case Stop:
+      Serial.println("Stopping vehicle...");
+      stop();
+      return Stopped;
+    case AvoidObstacle:
+      Serial.println("Avoiding obstacle...");
+      // Implementation for avoiding obstacle
+      return AvoidingObstacle;
+  }
 }
 
 void loop() {  
 
   if (digitalRead(BUTTON_PIN) == LOW) {
     if (currentState == Stopped) {
-      Serial.println("Driving forward...");      
-      driveForward();
-      currentState = Driving;
+      currentState = performAction(Start);
     } else {
-      Serial.println("stopped");
-      stop();
-      currentState = Stopped;
-    }    
-    
-    delay(300);    
+      currentState = performAction(Stop);
+    }               
   }  
+
+  delay(300); 
 }
