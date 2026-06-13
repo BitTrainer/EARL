06804b173e6ea251761268c7224dd203f3a74431
Bittrainer <grant.vanstaden@live.com.au>
2026-06-13 18:08:53 +1000

    Seperated determining cuurrent state and actioninging it.

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,53 +1,115 @@
+// main.ino
 #include "navigation_methods.h"
 
 enum VehicleState {
-  Stopped,
-  HeadingToDestination,
+  Waiting,
+  TurningAround,
+  HeadingToDestination,  
   AvoidingObstacle,
-  Arrived
+  Stuck,
 };
 
-enum VehicleAction{  
+enum VehicleEvent {  
   Start,
+  TurnAroundComplete,
   Stop,
-  AvoidObstacle
+  Continue,
+  AvoidObstacle,
+  WaitForAssistance
  };
 
 const int BUTTON_PIN = 9;  
-VehicleState currentState = Stopped;
+VehicleState currentState = Waiting;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+    Serial.begin(9600);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
   prepareVehicle();        
 }
 
-VehicleState performAction(VehicleAction action) {
-  switch(action) {    
+VehicleState determineState(VehicleEvent event) {
+
+  switch(event) {
+
     case Start:
-      Serial.println("Starting vehicle...");
-      driveForward();
-      return HeadingToDestination;
+      if (currentState == Waiting) {
+        return TurningAround;
+      }
+      break;
+
+    case TurnAroundComplete:
+      if (currentState == TurningAround) {
+        return HeadingToDestination;
+      }
+      break;
+
     case Stop:
-      Serial.println("Stopping vehicle...");
-      stop();
-      return Stopped;
+      if (currentState == HeadingToDestination) {
+        return Waiting;
+      }
+      break;
+
+    case Continue:
+      if (currentState == Stuck || currentState == AvoidingObstacle) {
+        return HeadingToDestination;
+      }
+      break;
+
     case AvoidObstacle:
-      Serial.println("Avoiding obstacle...");
-      // Implementation for avoiding obstacle
-      return AvoidingObstacle;
+      if (currentState == HeadingToDestination) {
+        return AvoidingObstacle;
+      }
+      break;
+
+    case WaitForAssistance:
+      if (currentState == AvoidingObstacle) {
+        return Stuck;
+      }
+      break;
   }
+
+  // No valid transition → remain in current state
+  return currentState;
+}
+
+void handleButtonPress() {  
+  if (currentState == Waiting) {
+      currentState = determineState(Start);
+  } else if (currentState == Stuck) {
+      currentState = determineState(Continue);
+  } else {
+      currentState = determineState(Stop);
+  }             
+}
+
+void actionCurrentState() {
+  switch(currentState) {
+    case Waiting:
+      stop();
+      break;
+    case TurningAround:
+      turnAround();
+      currentState = determineState(TurnAroundComplete);
+      break;
+    case HeadingToDestination:
+      driveForward();
+      break;    
+    case AvoidingObstacle:    
+      currentState = determineState(waitForAssistance);
+      break;
+    case Stuck:
+      stop();
+      break;
+  }
+
 }
 
 void loop() {  
-
-  if (digitalRead(BUTTON_PIN) == LOW) {
-    if (currentState == Stopped) {
-      currentState = performAction(Start);
-    } else {
-      currentState = performAction(Stop);
-    }               
+  if (digitalRead(BUTTON_PIN) == LOW) {    
+    handleButtonPress();  
   }  
 
-  delay(300); 
+  actionCurrentState();
+
+  delay(50); 
 }
